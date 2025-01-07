8900311e3bfb3b3ece73549f52f07e53bf634e75
AndreaDaLio <884046@stud.unive.it>
2025-01-07 18:50:03 +0100

    Init project

---
+++ src/main/java/com/git_commit_therapy/emergencyWardPanels/EmergencyWardPanelsApplication.java
@@ -0,0 +1,13 @@
+package com.git_commit_therapy.emergencyWardPanels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyWardPanelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyWardPanelsApplication.class, args);
+	}
+
+}
