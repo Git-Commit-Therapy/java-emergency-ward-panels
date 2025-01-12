b868e50e3a291f5007683d9e72bc0ab8147942da
AndreaDaLio <884046@stud.unive.it>
2025-01-12 14:49:39 +0100

    Allow requests to sites with self-signed certificates

--- src/main/java/com/git_commit_therapy/emergencyWardPanels/EmergencyWardPanelsApplication.java
+++ src/main/java/com/git_commit_therapy/emergencyWardPanels/EmergencyWardPanelsApplication.java
@@ -1,21 +1,65 @@
 package com.git_commit_therapy.emergencyWardPanels;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 @EnableScheduling
 @SpringBootApplication
 public class EmergencyWardPanelsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmergencyWardPanelsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyWardPanelsApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException {
+        TrustManager[] trustAllCertificates = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+                    }
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCertificates, new java.security.SecureRandom());
+        SSLConnectionSocketFactory socketFactory =  new SSLConnectionSocketFactory(sslContext);
+
+        HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .setSSLSocketFactory(socketFactory)
+                .build();
+
+        org.apache.hc.client5.http.impl.classic.CloseableHttpClient c5  = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .evictExpiredConnections()
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(c5);
+        return new RestTemplate(factory);
+    }
 }
